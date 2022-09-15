bbb7db264452e398f19e0641ad467aa1df278aa9
Nico <nicolas.villar@kzsoftworks.com>
2022-09-15 19:46:22 -0300

    Usar & para loadTime()

--- obligatorio/time.cpp
+++ obligatorio/time.cpp
@@ -1,11 +1,13 @@
+#include <stdio.h>
+
 #include "time.h"
 
 void loadTime(Time &time) {
     printf("Hora: ");
-    scanf("%d\r\n", time.Hours);
+    scanf("%d\r\n", &time.Hours);
 
     printf("Minutos: ");
-    scanf("%d\r\n", time.Minutes);
+    scanf("%d\r\n", &time.Minutes);
 }
 
 int getHours(Time time) {
