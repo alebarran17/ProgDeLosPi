ac121779e2c3d81fd7e7c332121d3dd2fbd7e748
nicoovillarr <nicolasvillar00@gmail.com>
2022-11-18 20:06:12 -0300

    Add project dependencies and remove unwanted code from main.

--- obligatorio2/main.cpp
+++ obligatorio2/main.cpp
@@ -1,18 +1,7 @@
 #include <locale.h>
 
-#include "expediente.h"
-#include "revision.h"
-
 int main()
 {
     setlocale(LC_ALL, "spanish");
-    Expediente e;
-    CargarExpediente(e);
-    MostrarExpediente(e);
-    printf("\r\n");
-
-    Revision r;
-    CargarRevision(r);
-    MostrarRevision(r);
     return 0;
 }
