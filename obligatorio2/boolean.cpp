7def93b5990c4c8bd9ca74657a83525a04e28ae3
Nicolás Villar <nicolas.villar@kzsoftworks.com>
2022-11-24 19:18:56 -0300

    Fix boolean.cpp indent

--- obligatorio2/boolean.cpp
+++ obligatorio2/boolean.cpp
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 void carga(boolean &b){
-                int num;
-                printf("0-No hay 1-Hay:\n");
-                scanf("%d", &num);
-                if(num==1)
-                                b=TRUE;
-                else
-                                b=FALSE;
+    int num;
+    printf("0-No hay 1-Hay:\n");
+    scanf("%d", &num);
+    if(num==1)
+        b = TRUE;
+    else
+        b = FALSE;
 }
 void mostrar(boolean b){
-                if(b==TRUE)
-                                printf("Si");
-                else
-                                printf("No");
+    if(b==TRUE)
+        printf("Si");
+    else
+        printf("No");
 }
