a44d698b6b16ea3568b001cffdfe4397e5b076dd
alebarran17 <87735088+alebarran17@users.noreply.github.com>
2022-11-25 19:15:40 -0300

    Funciones tienen descripcion

--- obligatorio2/listaRevisiones.h
+++ obligatorio2/listaRevisiones.h
@@ -11,34 +11,50 @@ typedef struct nodoR {
 
 typedef ListaRevision * Lista;
 
+//Crea una lista vacia.
 void CrearListaRevision(Lista &L);
 
+//Imprime la lista.
 void MostrarLista(Lista root);
 
+//Imprime una revision dado un codigo de expediente.
 void MostrarLista(Lista root, int expedienteId);
 
+//Devuelve TRUE si la lista es vacia, FALSE en caso contrario.
 boolean VaciaListaRevision(Lista L);
 
-/* Precondición: lista NO vacía */
+//Devuevle el primero de la lista.
+/* PrecondiciÃ³n: lista NO vacÃ­a */
 Revision Primero (Lista L);
 
-/* Precondición: lista NO vacía */
+//Devuelve del segundo en adelante.
+/* PrecondiciÃ³n: lista NO vacÃ­a */
 void Resto (Lista & L);
 
+//Inserta una revision en una lista.
 void InsertarRevision (Lista & L, Revision R);
 
+//Guarda la lista en una archivo
+//Precondicion: El archivo debe venir abierto para escritura.
 void GuardarListaRevisiones(Lista L, FILE * f);
 
+//Lee una lista desde un archivo.
+//Precondicion: El archivo debe venir abierto para lectura.
 void LeerListaRevisiones(Lista &L, FILE * f);
 
+//Cuenta la cantidad de revisiones de la lista.
 int ContarRevisiones(Lista root);
 
+//Cuenta la cantidad de revisiones con el codigo ingresado
 int ContarRevisiones(Lista root, int expedienteId);
 
+//Cuenta la cantidad de revisiones Satisfactorias, Incompletas y pendientes.
 void ContarRevisiones(Lista root, int& s, int& i, int& p);
 
+//Cuenta las revisiones entre dos fechas.
 int ContarRevisiones(Lista root, Fecha desde, Fecha hasta);
 
+//Borra las revisiones dado un codigo de expediente.
 void BorrarRevisiones(Lista &from, int expedienteId);
 
 #endif // LISTAREVISIONES_H_INCLUDED
