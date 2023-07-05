3c5e33018d185959cd2eee2910eea4d3a31a8981
alebarran17 <87735088+alebarran17@users.noreply.github.com>
2023-07-05 19:31:47 -0300

    Agregar Carrera.cpp y Carrera.h

---
+++ ObligatorioP3/Carrera.cpp
@@ -0,0 +1,35 @@
+#include "Carrera.h"
+
+/// Operaciones primitivas.
+
+/// Crea una secuencia vacía.
+void Crear(Carrera &c){
+    c.tope = 0;
+}
+
+/// Devuelve la cantidad de elementos de la secuencia.
+int Largo(Carrera c){
+    return c.tope;
+}
+
+/// Devuelve el elemento que ocupa la posición K de la secuencia.
+/// Precondición: El largo de la secuencia es mayor o igual a K.
+Asignatura KEsimo(Carrera c, int k){
+    return c.arr[k];
+}
+
+/// Operaciones adicionales.
+
+///Agrega un elemento al final del secuencia.
+void InsBack(Carrera &c, Asignatura a){
+    c.arr[c.tope] = a;
+    c.tope++;
+}
+
+///Lista por pantalla la secuencia.
+void Listar(Carrera &c){
+    int i;
+    for(i = 0; i<c.tope; i++){
+        MostrarAsignatura(c.arr[i]);
+    }
+}
--- ObligatorioP3/Carrera.h
+++ ObligatorioP3/Carrera.h
@@ -12,19 +12,21 @@ typedef struct {
 /// Operaciones primitivas.
 
 /// Crea una secuencia vacía.
-Carrera Crear();
+void Crear(Carrera c);
 
 /// Devuelve la cantidad de elementos de la secuencia.
 int Largo(Carrera c);
 
 /// Devuelve el elemento que ocupa la posición K de la secuencia.
 /// Precondición: El largo de la secuencia es mayor o igual a K.
-Asignatura KEsimo(Carrera c, int n);
+Asignatura KEsimo(Carrera c, int k);
 
 /// Operaciones adicionales.
 
-void InsBack(Carrera c);
+///Agrega un elemento al final del secuencia.
+void InsBack(Carrera c, Asignatura a);
 
+///Lista por pantalla la secuencia.
 void Listar(Carrera c);
 
 #endif // ACTASIGNATURAS_H_INCLUDED
