89e7c9041c4d467428bfd85c14c7af94e02fcd9b
alebarran17 <87735088+alebarran17@users.noreply.github.com>
2023-07-13 21:25:18 -0300

    Comentar modulo Asignatura.h

--- ObligatorioP3/Asignatura.h
+++ ObligatorioP3/Asignatura.h
@@ -9,14 +9,19 @@ typedef struct {
     int horas;
 } Asignatura;
 
+///Carga la asignatura por teclado
 void CargarAsignatura(Asignatura& a);
 
+///Devuelve la ID de la asignatura
 int ObtenerIdAsignatura(Asignatura a);
 
+///Devuelve el nombre de la asignatura
 void ObtenerNombre(Asignatura a, String& s);
 
+///Devuelve la cantidad de horas de la asignatura
 int ObtenerHoras(Asignatura a);
 
+///Muestra la asignatura
 void MostrarAsignatura(Asignatura a);
 
 #endif // ASIGNATURA_H_INCLUDED
