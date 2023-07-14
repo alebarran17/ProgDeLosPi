91dab2c651188a72fc18d77d5482e938f4c98c56
alebarran17 <87735088+alebarran17@users.noreply.github.com>
2023-07-13 21:19:39 -0300

    Comentar Fecha.h
    Modificque el cabezal de FechaPosterior en el .h

--- ObligatorioP3/Fecha.h
+++ ObligatorioP3/Fecha.h
@@ -7,18 +7,25 @@ typedef struct {
     int anio;
 } Fecha;
 
+///Carga la fecha por teclado
 void CargarFecha(Fecha& f);
 
+///Muestra la fecha en pantalla
 void MostrarFecha(Fecha f);
 
+///Devuelve el dia de la fecha
 int ObtenerDiaFecha(Fecha f);
 
+///Devuelve el mes de la fecha
 int ObtenerMesFecha(Fecha f);
 
+///Devuelve el año de la fecha
 int ObtenerAnioFecha(Fecha f);
 
-bool FechaPosterior(Fecha a, Fecha b);
+///Determina si la fecha f1 es posterior a la f2
+bool FechaPosterior(Fecha f1, Fecha f2);
 
+///Determina si el formato de la fecha es correcto
 bool ValidarFormato(Fecha f);
 
 #endif // FECHA_H_INCLUDED
