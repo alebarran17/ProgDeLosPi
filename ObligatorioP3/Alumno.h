93465dc5a3533f7a589168299c67309d1ac0c413
alebarran17 <87735088+alebarran17@users.noreply.github.com>
2023-07-13 21:22:44 -0300

    Comentar modulo Alumno.h

--- ObligatorioP3/Alumno.h
+++ ObligatorioP3/Alumno.h
@@ -11,20 +11,28 @@ typedef struct {
     Escolaridad escolaridad;
 } Alumno;
 
+///Cargar el alumno por teclado
 void CargarAlumno(Alumno& a);
 
+///Devuelve la cedula del alumno
 long ObtenerCedulaAlumno(Alumno a);
 
+///Devuelve el nombre del alumno
 void ObtenerNombreAlumno(Alumno a, String& s);
 
+///Devuelve el apellido del alumno
 void ObtenerApellidoAlumno(Alumno a, String& s);
 
+///Devuelve el telefono del alumno
 void ObtenerTelefonoAlumno(Alumno a, String& s);
 
+///Devuelve la escolaridad del alumno
 Escolaridad ObtenerEscolaridadAlumno(Alumno a);
 
+///Modifica la escolaridad del alumno
 void SetEscolaridadAlumno(Alumno &a, Escolaridad e);
 
+///Muestra el alumno
 void MostrarAlumno(Alumno a);
 
 #endif // ALUMNO_H_INCLUDED
