6075c3a02249eff718d7f4ca3b6e24cd0b256974
alebarran17 <87735088+alebarran17@users.noreply.github.com>
2023-07-13 21:43:55 -0300

    Comentar String.h

--- ObligatorioP3/String.h
+++ ObligatorioP3/String.h
@@ -6,64 +6,58 @@
 const int MAX = 200;
 typedef char * String;
 
-//Crea el string con \0
+///Crea el string con \0
 void strcrear(String &s);
 
-//Devuelve el largo del string
+///Devuelve el largo del string
 int strlar(String s);
 
-//Copia el primer string al segundo
+///Copia el primer string al segundo
 void strcop(String from, String &to);
 
-//Lee un string desde teclado
+///Lee un string desde teclado
 void scan(String &s);
 
-//Concatena dos string
+///Concatena dos string
 void strcon(String &s1, String s2);
 
-//Cambia el primer string con el segundo
+///Cambia el primer string con el segundo
 void strswp(String &s1, String &s2);
 
-//Despliega una string por pantalla
+///Despliega una string por pantalla
 void print(String s1);
 
-//Devuelva TRUE si el primero de ellos es menor (en orden alfabético) que el segundo, o FALSE en caso contrario.
+///Devuelva TRUE si el primero de ellos es menor (en orden alfabético) que el segundo, o FALSE en caso contrario.
 bool strmen(String s1, String s2);
 
-//Devuelve TRUE si ambos strings son iguales o FALSE en caso contrario.
+///Devuelve TRUE si ambos strings son iguales o FALSE en caso contrario.
 bool streq(String s1, String s2, bool ignoreCase);
 
-// Precondición: El archivo viene abierto para escritura.
+/// Precondición: El archivo viene abierto para escritura.
 void GuardarString(String s, FILE * f);
 
-// Precondición: El archivo viene abierto para lectura.
-void LeerString(String &s, FILE * f);
-
-//Verifica que nombre sea alfabético
+///Verifica que nombre sea alfabético
 bool NombreAlfabetico(String nom);
 
-//Pasa a mayusculas el String
+///Pasa a mayusculas el String
 void PasarMayus(String &nombre);
 
-//Verifica que el parametro sea "->"
-bool EsFlecha(String flecha);
-
-//Libera el espacio de memoria usado por le string
+///Libera el espacio de memoria usado por le string
 void LiberarString(String &s);
 
 //Devuelve la primera palabra del string
 void PrimerPalabra(String &input, String &palabra);
 
-//Elimina los espacios que esten delante de la primer palabra.
+///Elimina los espacios que esten delante de la primer palabra.
 void Recortar(String &input);
 
-//Devuelve el string desde donde termina la primer palabra.
+///Devuelve el string desde donde termina la primer palabra.
 void Substr(String &input, int from);
 
-//Devuelve si el string es vacio
+///Devuelve si el string es vacio
 bool StringVacio(String input);
 
-//Valida que el formato de la fecha sea valido
+///Valida que el formato de la fecha sea valido
 bool ValidarFormato(String fecha);
 
 #endif // STRING_H_INCLUDED
